beb75e5c436e0d6e73e3cfb51c75fa797b5f6ee3
Andrey Belyaev <andrey.belyaev@pag.company>
2018-03-15 13:35:56 +0300

    Added loading store and removing one file benchmarks

--- src/PAG.FileStore.Runner/Program.cs
+++ src/PAG.FileStore.Runner/Program.cs
@@ -28,6 +28,15 @@ namespace PAG.FileStore.Runner
             Console.Write($"Searching store with {N} files... ");
             store.Add(new ExampleProduct { Id = Guid.NewGuid(), Name = "WhateverCheaper", Price = 11.00M }); ExampleProduct foundProduct = null;
             Console.Write("{0}ms - ", Benchmark(() => foundProduct = store.Where(p => p.Price == 11.00M).FirstOrDefault())); Console.WriteLine(foundProduct);
+
+            var store2 = new FileStore<ExampleProduct>(); // uses same file on disk
+
+            Console.Write($"Loading store with {N} files from disk... ");
+            Console.WriteLine("{0}ms", Benchmark(store2.Load));
+
+            Console.Write($"Removing last file from store with {N} files... ");
+            store2.FileRemoved += (s, e) => store2.Save();
+            Console.WriteLine("{0}ms", Benchmark(() => store2.Remove(foundProduct)));
         }
 
         private static void Populate(FileStore<ExampleProduct> store, int n)
